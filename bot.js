72d196f27d94884fff405427b9423965ea5d7706
Spam127 <45201082+Spam127@users.noreply.github.com>
2018-12-29 08:27:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 client.on("guildMemberAdd", member => {
         if(member.guild.id === "520166623395577857") {  
   const channel = member.guild.channels.find('526790967466917908'); 
